c418c5bbdda7af70a60707648093abb96c6128e3
Bardan Putra Prananto <bppdanto@gmail.com>
2025-02-03 11:32:07 +0300

    add RestTemplateLoggingInterceptor (#196)

--- src/main/java/org/eclipse/xpanse/terraform/boot/config/RestTemplateConfig.java
+++ src/main/java/org/eclipse/xpanse/terraform/boot/config/RestTemplateConfig.java
@@ -5,6 +5,9 @@
 
 package org.eclipse.xpanse.terraform.boot.config;
 
+import jakarta.annotation.Resource;
+import java.util.Collections;
+import org.eclipse.xpanse.terraform.boot.logging.RestTemplateLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -15,10 +18,14 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    @Resource RestTemplateLoggingInterceptor restTemplateLoggingInterceptor;
+
     /** Create RestTemplate to IOC. */
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        restTemplate.setInterceptors(Collections.singletonList(restTemplateLoggingInterceptor));
+        return restTemplate;
     }
 
     /** Create ClientHttpRequestFactory to IOC. */
---
+++ src/main/java/org/eclipse/xpanse/terraform/boot/logging/CustomClientHttpResponse.java
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ *
+ */
+
+package org.eclipse.xpanse.terraform.boot.logging;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.lang.NonNull;
+
+/**
+ * The class wraps a ClientHttpResponse and provides easy access to its properties as a byte array.
+ */
+public record CustomClientHttpResponse(
+        ClientHttpResponse originalResponse, byte[] responseBodyBytes)
+        implements ClientHttpResponse {
+
+    @NonNull
+    @Override
+    public HttpStatusCode getStatusCode() throws IOException {
+        return originalResponse.getStatusCode();
+    }
+
+    @NonNull
+    @Override
+    public String getStatusText() throws IOException {
+        return originalResponse.getStatusText();
+    }
+
+    @NonNull
+    @Override
+    public HttpHeaders getHeaders() {
+        return originalResponse.getHeaders();
+    }
+
+    @NonNull
+    @Override
+    public InputStream getBody() {
+        return new ByteArrayInputStream(responseBodyBytes);
+    }
+
+    @Override
+    public void close() {
+        originalResponse.close();
+    }
+}
