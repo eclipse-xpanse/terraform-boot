696d0a379d6134a64c8c3ceea09c81c06c3eae4d
swaroop.ar <swaroop.akulu.raghupathy@huawei.com>
2025-02-10 10:54:42 +0100

    add HMAC to supported authentication types

--- src/main/java/org/eclipse/xpanse/terraform/boot/models/enums/AuthType.java
+++ src/main/java/org/eclipse/xpanse/terraform/boot/models/enums/AuthType.java
@@ -5,8 +5,41 @@
 
 package org.eclipse.xpanse.terraform.boot.models.enums;
 
-/** The permission type class when calling back. */
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.xpanse.terraform.boot.models.exceptions.UnsupportedEnumValueException;
+
+/**
+ * The authentication type class when calling back. This is not used at moment. It is kept for
+ * future purpose when clients plan to support multiple authentication types in concurrently.
+ */
 public enum AuthType {
-    NONE,
-    OAUTH2;
+    NONE("none"),
+    OAUTH2("oauth"),
+    HMAC("hmac");
+
+    private final String authType;
+
+    AuthType(String authType) {
+        this.authType = authType;
+    }
+
+    /** Convert string to enum object. */
+    @JsonCreator
+    public AuthType getByValue(String value) {
+        for (AuthType authType : values()) {
+            if (authType.authType.equals(StringUtils.upperCase(value))) {
+                return authType;
+            }
+        }
+        throw new UnsupportedEnumValueException(
+                String.format("HealthStatus value %s is not supported.", value));
+    }
+
+    /** For RuntimeState deserialize. */
+    @JsonValue
+    public String toValue() {
+        return this.authType;
+    }
 }
